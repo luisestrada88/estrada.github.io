55116cacdb28f07d1454827b9574643f7dcaaecd
Ander Luna <zS23020596@estudiantes.uv.mx>
2025-04-04 17:40:09 -0600

    añadir carpeta models desde mi repo clonado

---
+++ src/app/models/education/education.model.ts
@@ -0,0 +1,7 @@
+export class Education {
+  id?: string;
+  carrera?: string = 'studio name';
+  educationHome?: string = 'formation name';
+  endDate?: string = 'end formation';
+  startDate?: string 'start formation';
+}
